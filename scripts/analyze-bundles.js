f67386b544d0c930489dae276d34cdd0c3cc3027
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2023-11-02 21:45:35 +0000

    feat: bundle analyze script

--- scripts/analyze-bundles.js
+++ scripts/analyze-bundles.js
@@ -2,19 +2,19 @@ const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib");
 
-const bundle = require("../.next/build-manifest.json");
+const currentBundle = require("../.next/app-build-manifest.json");
 
 const prefix = ".next";
-const outdir = path.join(process.cwd(), prefix, "analyze");
-const outfile = path.join(outdir, "bundle-sizes.txt");
+const outDir = path.join(process.cwd(), prefix, "analyze");
+const outFile = path.join(outDir, "bundle-sizes.txt");
 
-function formatBytes(bytes, signed = false) {
+const formatBytes = (bytes, signed = false) => {
   const sign = signed ? (bytes < 0 ? "-" : "+") : "";
   if (bytes === 0) return `${sign}0B`;
 
   const k = 1024;
   const dm = 2;
-  const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const sizes = ["B", "kB", "mB", "gB", "tB"];
 
   const i = Math.floor(Math.log(Math.abs(bytes)) / Math.log(k));
 
@@ -23,35 +23,78 @@ function formatBytes(bytes, signed = false) {
   }`;
 }
 
-const pageSizes = Object.keys(bundle.pages).map((p) => {
-  const files = bundle.pages[p];
+
+const renderTableRow = (files, append = 0) =>
+  files
+    .map(
+      ({ filename, size }) =>
+        `| \`${filename}\` | ${formatBytes(size)} | ${append ? `**${formatBytes(size + append)}**` : "" } |`,
+    )
+    .join("\n");
+
+const renderTitleTableRow = (title, size) =>`| \`${title}\` | ${formatBytes(size)} |  |`;
+
+
+const {["/layout"] : layoutFiles, ...pages} = currentBundle.pages
+
+const layoutFilesWithSizes = layoutFiles.reduce((files, filename) => {
+  const fn = path.join(process.cwd(), prefix, filename);
+  const bytes = fs.readFileSync(fn);
+  const zippedBytes = zlib.gzipSync(bytes);
+
+  return { ...files, [filename]: zippedBytes.byteLength };
+}, {})
+
+const jsLayoutFiles = [];
+const cssLayoutFiles = [];
+let indexLayoutSize = 0;
+
+for (const [filename, size] of Object.entries(layoutFilesWithSizes)) {
+  if (filename.startsWith("static/chunks/app/layout")) {
+    indexLayoutSize = size;
+  } else if (filename.endsWith(".css")) {
+    cssLayoutFiles.push({ filename, size });
+  } else if (filename.endsWith(".js")) {
+    jsLayoutFiles.push({ filename, size });
+  }
+}
+
+const totalJsLayoutSize = jsLayoutFiles.reduce((s, { size }) => s + size, 0);
+const totalCssLayoutSize = cssLayoutFiles.reduce((s, { size }) => s + size, 0);
+
+const pageSizes = Object.keys(pages).map((page) => {
+  const files = currentBundle.pages[page];
   const size = files
     .map((filename) => {
+      if (layoutFilesWithSizes[filename]) return 0;
+
       const fn = path.join(process.cwd(), prefix, filename);
       const bytes = fs.readFileSync(fn);
-      const gzipped = zlib.gzipSync(bytes);
-      return gzipped.byteLength;
+      const zippedBytes = zlib.gzipSync(bytes);
+      return zippedBytes.byteLength;
     })
     .reduce((s, b) => s + b, 0);
 
-  return { path: p, size };
+  return { filename: page.replace("/page", "") || "/", size };
 });
 
-// Produce a Markdown table with each page & its size
-const sizes = pageSizes
-  .map(({ path, size }) => `| \`${path}\` | ${formatBytes(size)} |`)
-  .join("\n");
 
+// Produce a Markdown table with each path, chunk and their size
 const output = `# Bundle Size
-| Route | Size (gzipped) |
-| --- | --- |
-${sizes}
+| Route | Size | Total size |
+| --- | --- | ---: |
+${renderTableRow(pageSizes, totalJsLayoutSize)}
+${renderTitleTableRow("JS Shared by all pages", totalJsLayoutSize)}
+${renderTableRow(jsLayoutFiles)}
+| --- | --- | ---: |
+${renderTitleTableRow("CSS Shared by all pages", totalCssLayoutSize)}
+${renderTableRow(cssLayoutFiles)}
 <!-- GH BOT -->`;
 
 try {
-  fs.mkdirSync(outdir);
+  fs.mkdirSync(outDir);
 } catch (e) {
-  // may already exist
+  // Ignore (dir exists)
 }
 
-fs.writeFileSync(outfile, output);
+fs.writeFileSync(outFile, output);
