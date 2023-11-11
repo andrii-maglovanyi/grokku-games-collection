2c8a025c2ae8fdc12c5d12dda8474065e2e94d3f
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2023-11-11 08:07:55 +0000

    new page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Help() {
+export default function Contacts() {
   return (
     <main className="justify-top flex min-h-screen flex-col items-center p-24">
-      <h1 className="mb-8 text-4xl font-bold">Help</h1>
+      <h1 className="mb-8 text-4xl font-bold">Contacts</h1>
     </main>
   );
 }
