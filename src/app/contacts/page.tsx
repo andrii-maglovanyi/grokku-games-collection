04beb33c8c986f48851dbafef97b3cc2a59e2266
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2023-11-11 08:07:55 +0000

    new page

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Contacts() {
+  return (
+    <main className="justify-top flex min-h-screen flex-col items-center p-24">
+      <h1 className="mb-8 text-4xl font-bold">Contacts here</h1>
+      <h1 className="mb-8 text-4xl font-bold">Contacts here</h1>
+      <h1 className="mb-8 text-4xl font-bold">Contacts here</h1>
+      <h1 className="mb-8 text-4xl font-bold">Contacts here</h1>
+      <h1 className="mb-8 text-4xl font-bold">Contacts here</h1>
+      <h1 className="mb-8 text-4xl font-bold">Contacts here</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="justify-top flex min-h-screen flex-col items-center p-24">
       <h1 className="mb-8 text-4xl font-bold">Grokku games collection</h1>
-      <Link href="/memory">Memory</Link>
+      {/* <Link href="/memory">Memory</Link> */}
     </main>
   );
 }
